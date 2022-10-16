4b30e75e24a362cd806f2817913e01c87b71f1aa
rundon <1058950205@qq.com>
2022-10-16 23:37:06 +0800

    feat:升级mybatisplus

--- oauth2/src/main/java/com/onefly/united/oauth2/service/impl/AuthencationFailureListener.java
+++ oauth2/src/main/java/com/onefly/united/oauth2/service/impl/AuthencationFailureListener.java
@@ -7,6 +7,7 @@ import com.onefly.united.common.constant.LogMessageDto;
 import com.onefly.united.common.constant.LoginOperationEnum;
 import com.onefly.united.common.constant.LoginStatusEnum;
 import com.onefly.united.common.utils.IpUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,6 +20,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Slf4j
 @Component
 public class AuthencationFailureListener implements ApplicationListener<AbstractAuthenticationFailureEvent> {
 
@@ -30,45 +32,50 @@ public class AuthencationFailureListener implements ApplicationListener<Abstract
 
     @Override
     public void onApplicationEvent(AbstractAuthenticationFailureEvent event) {
-        LogMessageDto log = new LogMessageDto();
-        log.setLogType("01");
+        LogMessageDto msg = new LogMessageDto();
+        msg.setLogType("01");
         Map<String, Object> data = Maps.newHashMap();
         //提供的凭据是错误的，用户名或者密码错误
         if (event instanceof AuthenticationFailureBadCredentialsEvent) {
             Map user = (LinkedHashMap) event.getAuthentication().getDetails();
-            String username = (String) user.get("username");
-            if (username.contains(":")) {
-                username = username.substring(0, username.length() - 1);
+            if (user != null) {
+                String username = (String) user.get("username");
+                if (username.contains(":")) {
+                    username = username.substring(0, username.length() - 1);
+                }
+                data.put("ip", IpUtils.getIpAddr(request));
+                data.put("userAgent", request.getHeader(HttpHeaders.USER_AGENT));
+                data.put("operation", LoginOperationEnum.LOGIN.value());
+                data.put("status", LoginStatusEnum.FAIL.value());
+                data.put("creatorName", username);
+                data.put("createDate", new Date());
+                msg.setData(data);
+                redisTemplate.convertAndSend(Constant.LOG_CHANNEL_TOPIC, JSON.toJSONString(msg));
+            } else {
+                log.error("用户名为空");
             }
-            data.put("ip", IpUtils.getIpAddr(request));
-            data.put("userAgent", request.getHeader(HttpHeaders.USER_AGENT));
-            data.put("operation", LoginOperationEnum.LOGIN.value());
-            data.put("status", LoginStatusEnum.FAIL.value());
-            data.put("creatorName", username);
-            data.put("createDate", new Date());
-            log.setData(data);
-            redisTemplate.convertAndSend(Constant.LOG_CHANNEL_TOPIC, JSON.toJSONString(log));
+
         } else if (event instanceof AuthenticationFailureCredentialsExpiredEvent) {
             //验证通过，但是密码过期
-            System.out.println("---AuthenticationFailureCredentialsExpiredEvent---");
+            log.error("---AuthenticationFailureCredentialsExpiredEvent---");
         } else if (event instanceof AuthenticationFailureDisabledEvent) {
             //验证过了但是账户被禁用
-            System.out.println("---AuthenticationFailureDisabledEvent---");
+            log.error("---AuthenticationFailureDisabledEvent---");
         } else if (event instanceof AuthenticationFailureExpiredEvent) {
             //验证通过了，但是账号已经过期
-            System.out.println("---AuthenticationFailureExpiredEvent---");
+            log.error("---AuthenticationFailureExpiredEvent---");
         } else if (event instanceof AuthenticationFailureLockedEvent) {
             //账户被锁定
-            System.out.println("---AuthenticationFailureLockedEvent---");
+            log.error("---AuthenticationFailureLockedEvent---");
         } else if (event instanceof AuthenticationFailureProviderNotFoundEvent) {
             //配置错误，没有合适的AuthenticationProvider来处理登录验证
-            System.out.println("---AuthenticationFailureProviderNotFoundEvent---");
+            log.error("---AuthenticationFailureProviderNotFoundEvent---");
         } else if (event instanceof AuthenticationFailureProxyUntrustedEvent) {
             //代理不受信任，用于Oauth、CAS这类三方验证的情形，多属于配置错误
-            System.out.println("---AuthenticationFailureProxyUntrustedEvent---");
+            log.error("---AuthenticationFailureProxyUntrustedEvent---");
         } else if (event instanceof AuthenticationFailureServiceExceptionEvent) {
             //其他任何在AuthenticationManager中内部发生的异常都会被封装成此类
-            System.out.println("---AuthenticationFailureServiceExceptionEvent---");
+            log.error("---AuthenticationFailureServiceExceptionEvent---");
         }
     }
 }
